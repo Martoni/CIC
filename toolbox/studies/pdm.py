6bc2e0ea4b8cb8ba7cdfd40922be0191f82c8649
Fabien Marteau <fabien.marteau@armadeus.com>
2021-06-14 21:22:53 +0200

    python pdm experimentations

---
+++ toolbox/studies/pdm.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+# Generate simple sinus 
+n = 400
+fclk = 4e6
+fsound = 20000
+t = np.arange(n)/fclk
+x = 0.5 + 0.5*np.sin(2*np.pi*fsound*t)
+
+def pdm(values):
+    n = len(values)
+    pdmval = np.zeros(n)
+    error = np.zeros(n+1)
+    for i in range(n):
+       pdmval[i] = 1 if values[i] >= error[i] else 0
+       error[i+1] = pdmval[i] - values[i] + error[i]
+    return pdmval, error[:n] 
+
+def slipping_mean(values, meansize):
+    n = len(values)
+    meanvals = np.zeros(n+meansize)
+    for i in range(n):
+        meanvals[i] = np.mean(x[i-meansize:i])
+    return meanvals
+
+meansize = 10
+meanvals = slipping_mean(x, meansize)
+
+# plot pdm signal
+pdmval, error = pdm(x)
+
+#plt.step(t, error)
+plt.step(t, pdmval)
+# print sinus
+plt.plot(t, x)
+# print slipping mean
+halfmeansize = int(meansize/2)
+print(f"halfmeansize {halfmeansize}")
+plt.plot(t, meanvals[halfmeansize:-(halfmeansize)])
+
+
+
+plt.show()
