478f4912d60a7ad231f79ddc84835c80837baa29
Fabien Marteau <fabien.marteau@armadeus.com>
2021-07-04 08:37:12 +0200

    generate audio wave file

--- toolbox/studies/csv2np.py
+++ toolbox/studies/csv2np.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import wave
+import struct
 import numpy as np
 import getopt
 
@@ -25,6 +27,29 @@ class Csv2Np(object):
 
     def write_np(self):
         np.save(self.npfilename, self.npoutput)
+
+    def read_np(self):
+        self.npoutput = np.load(self.npfilename)
+
+    def write_sound(self):
+        freq = 3000000
+        sound_sampling = 44100
+        decimate = int(freq/sound_sampling)
+        # mean and decimate
+        alist = []
+        full_size = len(self.npoutput)
+        for i in range(int(full_size/decimate)):
+            alist.append(sum(self.npoutput[i*decimate:(i+1)*decimate]))
+        # save as sound
+        obj = wave.open(f"{self.npfilename}.wav", "w")
+        obj.setnchannels(1) # mono
+        obj.setsampwidth(2)
+        obj.setframerate(sound_sampling)
+        for value in alist:
+            data = struct.pack('<h', (value-35)*5000)
+            obj.writeframesraw(data)
+        obj.close() 
+
 
 def usage():
     print("Convert pdm csv captured with pulseview to numpy array")
@@ -53,11 +78,14 @@ if __name__ == "__main__":
             usage()
             sys.exit(2)
 
-    if None in [csvfilename, npfilename]:
-        print("Give filenames")
-        usage()
-        sys.exit(2)
-		 
-    c2n = Csv2Np(csvfilename, npfilename)
-    c2n.read_csv()
-    c2n.write_np()
+    if not None in [csvfilename, npfilename]:
+        print(f"Convert csv {csvfilename} to np file {npfilename}.npy")
+        c2n = Csv2Np(csvfilename, npfilename)
+        c2n.read_csv()
+        c2n.write_np()
+
+    elif (csvfilename is None) and (npfilename is not None):
+        print(f"Filter np file and write sound file {npfilename}.wav")
+        c2n = Csv2Np(csvfilename=None, npfilename=npfilename)
+        c2n.read_np()
+        c2n.write_sound()
