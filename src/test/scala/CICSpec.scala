622539f076551bc1abea123c306ba7d0828bf6df
Fabien Marteau <fabien.marteau@armadeus.com>
2021-08-21 07:36:35 +0200

    begin test

--- src/test/scala/CICSpec.scala
+++ src/test/scala/CICSpec.scala
@@ -1,1 +1,13 @@
 package cic
+
+import org.scalatest._
+import chiseltest._
+import chisel3._
+
+class BasicTest extends FlatSpec with ChiselScalatestTester with Matchers {
+  behavior of "CIC"
+  // test class body here
+  it should "filter pdm signal file " in {
+    println("hello")
+  }
+}
