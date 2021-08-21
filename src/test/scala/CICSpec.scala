dc767368a59177fae982fb65cfeb65cc9614a309
username <emailaddress@yahoo.com>
2021-08-21 08:29:39 +0200

    trying to open csv file

--- src/test/scala/CICSpec.scala
+++ src/test/scala/CICSpec.scala
@@ -5,9 +5,16 @@ import chiseltest._
 import chisel3._
 
 class BasicTest extends FlatSpec with ChiselScalatestTester with Matchers {
-  behavior of "CIC"
-  // test class body here
-  it should "filter pdm signal file " in {
-    println("hello")
-  }
+    behavior of "CIC"
+
+    val csvpath: String = "assets/flute_si_12_5Mhz.csv"
+
+    it should "filter pdm signal file " + csvpath in {
+        println("Open file " + csvpath)
+        val fcsv = io.Source.fromFile(csvpath)
+        val itcsv = fcsv.getLines()
+        val title_line = itcsv.next().split(',')
+        println()
+        fcsv.close()
+    }
 }
