f13145aaf60ae65b2707a638d3a626caa6834c47
Fabien Marteau <fabien.marteau@armadeus.com>
2021-08-26 13:42:07 +0200

    using raw binary pdm file

--- src/test/scala/CICSpec.scala
+++ src/test/scala/CICSpec.scala
@@ -12,45 +12,19 @@ import java.io._ // for files read/write access
 class BasicTest extends FlatSpec with ChiselScalatestTester with Matchers {
     behavior of "CIC"
 
-    val csvpath: String = "assets/flute_si_12_5Mhz.csv"
+    val pdmpath: String = "assets/colamonptitfrere_12.5Ms.pdm"
 
-    it should "filter pdm signal file " + csvpath + s" and record $csvpath.pcm" in {
-        println("Open file " + csvpath)
-        val fcsv = io.Source.fromFile(csvpath)
-        val itcsv = fcsv.getLines()
-        // Get header titles
-        val Array(atime, aclk, adata) = itcsv.next().split(',')
-        println(s"time $atime, clk $aclk, data  $adata")
-        val csvseq = itcsv.toSeq
-        val csvseqsize = csvseq.size
-        println(s"$csvpath have $csvseqsize lines")
-
-
-        // convert csv to (clk,data) sequence
-        val rawsig: Seq[(Int,Int)] = {
-            csvseq.map { line =>
-                val Array(btime, bclk, bdata) = line.split(',').map(_.trim.toInt)
-                (bclk, bdata)
-            }
-        }
-
-        // get pdm bitstream
-        val pdm: Seq[Int]  = rawsig.zipWithIndex.map {
-                case(a, i) => if(i != 0){ (rawsig(i-1),a) } else ((0,0), a)
-            }.filter {
-                case ((clkold, dataold), (clk, data)) => clkold == 0 && clk == 1
-                case _ => false
-            }.map{
-                case ((_, _), (_, adata)) => adata
-                case _ => 0
-            }
-
+    it should "filter pdm signal file " + pdmpath + s" and record $pdmpath.pcm" in {
+        println("Open file " + pdmpath)
+        val bis = new BufferedInputStream(new FileInputStream(pdmpath))
+        val pdm = Stream.continually(bis.read).takeWhile(-1 != _).map(_.toByte).toArray
+        bis.close
 
         // test CIC
-       object MyCICParams extends CICParams(5, 68, 1)
-        val pcmFile = new BufferedOutputStream(new FileOutputStream(s"$csvpath.pcm"))
-        //test(new CIC(R=68)).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
-        test(new CIC(c=MyCICParams)) { dut =>
+        object MyCICParams extends CICParams(3, 68, 1)
+        val pcmFile = new BufferedOutputStream(new FileOutputStream(s"$pdmpath.pcm"))
+        test(new CIC(c=MyCICParams)).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
+        //test(new CIC(c=MyCICParams)) { dut =>
             println("begin test")
             // init input
             dut.io.pdm.clk.poke(false.B)
@@ -60,26 +34,24 @@ class BasicTest extends FlatSpec with ChiselScalatestTester with Matchers {
             dut.clock.step(1)
             var cyclenum = 0
             var pcmsamples = 0
-            breakable {for(data <- pdm) {
+            breakable{for(data <- pdm) {
                 dut.io.pdm.clk.poke(false.B)
                 dut.io.pdm.data.poke(data.B)
                 dut.clock.step(1)
                 dut.io.pdm.clk.poke(true.B)
                 if(dut.io.pcm.valid.peek().litValue() == 1){
                     val pcmvalue = dut.io.pcm.bits.peek().litValue()
-                    pcmFile.write(pcmvalue.toByteArray)
-                    if(pcmsamples > 1000) {
-                      break
-                    }
+                    val pcmarray = pcmvalue.toByteArray
+                    pcmFile.write((Array[Byte](0,0) ++ pcmarray).takeRight(2))
                     pcmsamples += 1
-                    if(pcmsamples % 10 == 0)
+                    if(pcmsamples % 1000 == 0)
                       println(s"pcmsamples $pcmsamples -> $pcmvalue")
+                    if(pcmsamples > 45000)break;
                 }
                 dut.clock.step(1)
                 cyclenum += 1
             }}
         }
         pcmFile.close()
-        fcsv.close()
     }
 }
