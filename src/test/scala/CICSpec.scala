4ee53b2d464a2f920cfa96d851bfcfd66f644c93
Fabien Marteau <fabien.marteau@armadeus.com>
2021-08-27 15:36:48 +0200

    adding some memory experimentation code for chiseltest

--- src/test/scala/CICSpec.scala
+++ src/test/scala/CICSpec.scala
@@ -8,17 +8,34 @@ import chiseltest.experimental.TestOptionBuilder._
 import chiseltest.internal.VerilatorBackendAnnotation
 import scala.util.control.Breaks._
 import java.io._ // for files read/write access
+import scala.util.Random
 
 class BasicTest extends FlatSpec with ChiselScalatestTester with Matchers {
     behavior of "CIC"
 
     val pdmpath: String = "assets/colamonptitfrere_12.5Ms.pdm"
+    val runtime = Runtime.getRuntime
+    val randbool = Random
+
+    def printMemoryUsage(): Unit = {
+        val mb = 1024*1024
+        println("** Used Memory:  " + getMemoryUsage() + " MB")
+        println("** Free Memory:  " + runtime.freeMemory/mb + "MB")
+        println("** Total Memory: " + runtime.totalMemory/mb + "MB")
+        println("** Max Memory:   " + runtime.maxMemory/mb + "MB")
+    }
+
+    def getMemoryUsage(): Long = {
+        val mb = 1024*1024
+        (runtime.totalMemory - runtime.freeMemory)/mb
+    }
 
     it should "filter pdm signal file " + pdmpath + s" and record $pdmpath.pcm" in {
         println("Open file " + pdmpath)
         val bis = new BufferedInputStream(new FileInputStream(pdmpath))
-        val pdm = Stream.continually(bis.read).takeWhile(-1 != _).map(_.toByte).toArray
-        val pcmFile = new BufferedOutputStream(new FileOutputStream(s"$pdmpath.pcm"))
+        //val pdm = Stream.continually(bis.read).takeWhile(-1 != _).map(_.toByte).toArray
+        val pdm = for(i <- 0 to 24*1024*1024) yield if(randbool.nextBoolean) {1} else {0}
+        //val pcmFile = new BufferedOutputStream(new FileOutputStream(s"$pdmpath.pcm"))
 
         // test CIC
         object MyCICParams extends CICParams(3, 68, 1)
@@ -41,17 +58,19 @@ class BasicTest extends FlatSpec with ChiselScalatestTester with Matchers {
                 if(dut.io.pcm.valid.peek().litValue() == 1){
                     val pcmvalue = dut.io.pcm.bits.peek().litValue()
                     val pcmarray = pcmvalue.toByteArray
-                    pcmFile.write((Array[Byte](0,0) ++ pcmarray).takeRight(2))
+                    //pcmFile.write((Array[Byte](0,0) ++ pcmarray).takeRight(2))
                     pcmsamples += 1
-                    if(pcmsamples % 1000 == 0)
-                      println(s"pcmsamples $pcmsamples -> $pcmvalue")
-                    if(pcmsamples > 45000)break;
+                    if(pcmsamples % 1000 == 0){
+                      val memUsage = getMemoryUsage()
+                      println(s"Mem: $memUsage,     |pcmsamples $pcmsamples -> $pcmvalue. Mem: $memUsage,")
+                    }
+                    if(pcmsamples > 80000)break;
                 }
                 dut.clock.step(1)
                 cyclenum += 1
             }}
         }
-        pcmFile.close()
+        //pcmFile.close()
         bis.close
     }
 }
